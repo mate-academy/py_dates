5f3d2a05f7ca5980aaae8e744a97259a40cc851c
vlysenok <tina.lysenok@gmail.com>
2019-11-29 12:25:18 +0200

    hw10_py_dates

--- dates.py
+++ dates.py
@@ -1,7 +1,14 @@
-import datetime
+"""module date"""
+from datetime import datetime
 
 
-def dates_between(d1: str, d2: str) -> int:
-
-    return 0
-
+def dates_between(frst_date: str, sec_date: str) -> int:
+    """
+    Calculate the difference between dates in days.
+    Dates format should be DD.MM.YYYY
+    :param frst_date: str
+    :param sec_date: str
+    :return: int
+    """
+    date_diff = datetime.strptime(sec_date, "%d.%m.%Y") - datetime.strptime(frst_date, "%d.%m.%Y")
+    return abs(date_diff.days)
