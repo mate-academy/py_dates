13195a848bef9bc164c242d96b608df63783269c
koreyo <KoretsValentyn@gmail.com>
2019-12-02 12:26:46 +0200

    diff

--- dates.py
+++ dates.py
@@ -1,7 +1,8 @@
-import datetime
+"""docstring"""
+from datetime import datetime
 
 
-def dates_between(d1: str, d2: str) -> int:
-
-    return 0
-
+def dates_between(date1: str, date2: str) -> int:
+    """return difference between two date in days"""
+    return (datetime.strptime(date2, "%d.%m.%Y")
+            - datetime.strptime(date1, "%d.%m.%Y")).days
