7efc15ec34a1f82f6d7e41aaf7b3305923511c85
Andrii <andrewsotnikov88@gmail.com>
2019-12-02 22:47:31 +0200

    Difference between two date

--- dates.py
+++ dates.py
@@ -1,7 +1,9 @@
-import datetime
+"""Calculate the difference between these dates in days."""
+from datetime import datetime
 
 
-def dates_between(d1: str, d2: str) -> int:
-
-    return 0
-
+def dates_between(date_one: str, date_two: str) -> int:
+    """ Difference between two datetime objects"""
+    date1 = datetime.strptime(date_one, "%d.%m.%Y")
+    date2 = datetime.strptime(date_two, "%d.%m.%Y")
+    return abs((date2 - date1).days)
